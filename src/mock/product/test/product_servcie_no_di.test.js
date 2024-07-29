f4d40d6539048cf2112a61bab878d5678650273b
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-29 15:35:51 +0900

    feat: mock을 이용한 제품 정보 가져오기 (test)

--- src/mock/product/test/product_servcie_no_di.test.js
+++ src/mock/product/test/product_servcie_no_di.test.js
@@ -1,22 +1,26 @@
 const ProductService = require("../product_service_no_di");
 const ProductClient = require("../product_client");
 
-jest.mock("../product_client");
 
+jest.mock('../product_client');
 describe("ProductService", () => {
-  const fetchItems = jest.fn(async () => [
-    { item: "Milk", available: true },
-    { item: "Banana", available: false },
-  ]);
-  let productService;
+  const fetchItems = jest.fn(async () => {
+    return [
+      { item: '🥛', available: true },
+      { item: '🍌', available: false }
+    ]
+  });
+
   ProductClient.mockImplementation(() => {
     return {
       fetchItems,
-    };
-  });
-  let callback;
+    }
+  })
+  let productService;
+
   beforeEach(() => {
     productService = new ProductService();
+    // clearMocks related...
     fetchItems.mockClear();
     ProductClient.mockClear();
   });
@@ -24,11 +28,11 @@ describe("ProductService", () => {
   it("should filter out only available items", async () => {
     const items = await productService.fetchAvailableItems();
     expect(items.length).toBe(1);
-    expect(items).toEqual([{ item: "Milk", available: true }]);
+    expect(items).toEqual([{ item: '🥛', available: true }]);
   });
 
-  it("test", async () => {
+  it('test', async () => {
     const items = await productService.fetchAvailableItems();
     expect(fetchItems).toHaveBeenCalledTimes(1);
-  });
-});
+  })  
+})
