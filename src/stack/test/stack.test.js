d7b25f316f114fd9e39bb95fd5a603c84793ef94
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-30 16:19:16 +0900

    feat: tdd 를 통한 stack test code 작성

---
+++ src/stack/test/stack.test.js
@@ -0,0 +1,47 @@
+const Stack = require('../stack.js');
+
+describe('Stack', () => {
+    let stack;
+
+    beforeEach(() => {
+        stack = new Stack();
+    });
+
+    it('is created empty', () => {
+        expect(stack.size()).toBe(0);
+    });
+
+    it('allow to push item', () => {
+        stack.push('🥪');
+        expect(stack.size()).toBe(1);
+    });
+
+    describe('pop', () => {
+        it('throws an error if stack is empty', () => {
+            expect(() => { stack.pop() }).toThrow('Stack is empty');
+        });
+
+        it('returns the last pushed item and remove it from the stack', () => {
+            stack.push('🍚');
+            stack.push('🍟');
+
+            expect(stack.pop()).toBe('🍟');
+            expect(stack.size()).toBe(1);
+
+        })
+    });
+
+    describe('peek', () => {
+        it('throws an error if stack is empty', () => {
+            expect(() => { stack.peek() }).toThrow('Stack is empty');
+        });
+        it('returns the last pushed item but keeps it in the stack', () => {
+            stack.push('🍚');
+            stack.push('🍟');
+
+            expect(stack.peek()).toBe('🍟');
+            expect(stack.size()).toBe(2);
+        });
+
+    })
+});
